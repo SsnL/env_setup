985d0d68d6bd1afcf7efe34e4f8de94eee6b7303
Tongzhou Wang <tongzhou@visiongpu31.csail.mit.edu>
2018-11-09 03:24:42 -0500

    Initial script

---
+++ setup.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+DIR=$(mktemp -d /tmp/foo.XXXXXXXXX)
+
+pushd $DIR
+
+# zsh
+if [[ -z $(command -v zsh) ]]; then
+  apt install zsh
+  chsh -s $(which zsh)
+fi
+
+# oh-my-zsh
+if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+# miniconda3
+# TODO: check version
+if [[ -z $(command -v conda) ]]; then
+  wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ./miniconda.sh
+  bash ./miniconda.sh -b -p $HOME/miniconda3
+
+  cat <<EOT >> $HOME/.zshrc
+# miniconda
+export PATH=\$HOME/miniconda3/bin:\$PATH" >> $HOME/.zshrc
+EOT
+  
+  source $HOME/.zshrc
+fi
+
+# tmux plugin manager
+# tmux better mouse mode
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+cat <<EOT >> $HOME/.tmux.conf 
+# List of plugins
+set -g @plugin 'tmux-plugins/tpm'
+set -g @plugin 'tmux-plugins/tmux-sensible'
+set -g @plugin 'nhdaly/tmux-better-mouse-mode'
+
+# Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)
+run -b '~/.tmux/plugins/tpm/tpm'
+
+set-option -g mouse on
+set-option -g history-limit 20000
+EOT
+tmux source ~/.tmux.conf
+
+# TODO: gdb
+
+popd
