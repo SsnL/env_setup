c23eaa3002cda9a719d36644ed53acc986486971
Tongzhou Wang <SsnL@users.noreply.github.com>
2020-05-13 16:52:23 -0400

    easydict

--- setup.sh
+++ setup.sh
@@ -309,7 +309,7 @@ as_real_user conda install jupyter ipython numpy scipy yaml matplotlib scikit-im
                            ninja tqdm imageio psutil -y
 as_real_user conda install -c conda-forge jupyter_contrib_nbextensions -y
 as_real_user conda install -c pytorch pytorch torchvision cudatoolkit=10.0 -y  # TODO: Is using 10.0 here fine? No official 10.1 binary...
-as_real_user pip install -q oyaml codemod threadpoolctl ring
+as_real_user pip install -q oyaml codemod threadpoolctl ring easydict
 as_real_user conda install -c conda-forge ffmpeg imageio-ffmpeg -y
 as_real_user pip install -q dominate visdom opencv-python aiohttp setproctitle shortuuid
 as_real_user pip install -q pyvirtualdisplay gym pyro-ppl box2d-py roboschool atari-py
